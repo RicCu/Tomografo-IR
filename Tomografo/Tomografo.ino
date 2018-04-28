389ea2aee611bc676834f68fadc3c865b9262619
NadiaPGD <38802678+NadiaPGD@users.noreply.github.com>
2018-04-27 22:58:54 -0500

    Update Tomografo.ino

--- Tomografo/Tomografo.ino
+++ Tomografo/Tomografo.ino
@@ -1,23 +1,38 @@
 /*************************************************************************/
 
 /***********************CONSTANT DEFINITIONS******************************/
-#define NUM_DETECTORS 1
+#define NUM_DETECTORS 3
 #define NUM_SAMPLES_PER_DETECTOR 10
 #define NUM_STEPS_PER_SLICE 48 // 180 grados
+#define NUM_STEPS_PER_SLICE_Z 1 //30
 #define TOTAL_NUM_XY_STEPS 48 // 360
 #define CLOCKWISE_DIRECTION true
 #define BASE_DELAY 50
+
 /********************************Detectors**********************************/
-static const uint8_t DETECTOR_PINS[NUM_DETECTORS] = {A0};
+static const uint8_t DETECTOR_PINS[NUM_DETECTORS] = {A0, A1, A2};
 
 /********************************STEPPER**********************************/
 static const uint8_t STEPPER_XY_PINS[4] = {8, 9, 10, 11};
+static const uint8_t STEPPER_Z_PINS[4] = {2, 3, 4, 5};
 
-static const int STEP_SEQUENCE [ 4 ][ 4 ] =
+
+static const int STEP_SEQUENCE [ 8 ][ 4 ] =
    {  {1, 0, 0, 0},
+      {1, 1, 0, 0},
       {0, 1, 0, 0},
+      {0, 1, 1, 0},
       {0, 0, 1, 0},
+      {0, 0, 1, 1},
       {0, 0, 0, 1},
+      {1, 0, 0, 1}
+   };
+
+ static const int STEP_SEQUENCE_Z [ 4 ][ 4 ] =
+   {  {1, 0, 0, 0},
+      {0, 0, 1, 0},
+      {0, 1, 0, 0},
+      {0, 0, 0, 1}
    };
 
 /********************************COMMUNICATIONS**********************************/
@@ -59,8 +74,19 @@ void setupStepper(uint8_t* stepper_pins){
   pinMode(stepper_pins[3], OUTPUT);
 }
 
+void setupStepperZ(uint8_t* stepper_pinsZ){
+  pinMode(stepper_pinsZ[0], OUTPUT);
+  pinMode(stepper_pinsZ[1], OUTPUT);
+  pinMode(stepper_pinsZ[2], OUTPUT);
+  pinMode(stepper_pinsZ[3], OUTPUT);
+}
+
 int stepXY(uint8_t* stepper_pins, unsigned int current_step, bool clockwise){
-  int step_stage = current_step % 4;
+  int step_stage = current_step % 8;
+  digitalWrite( stepper_pins[0], STEP_SEQUENCE[step_stage][ 0] );
+  digitalWrite( stepper_pins[1], STEP_SEQUENCE[step_stage][ 1] );
+  digitalWrite( stepper_pins[2], STEP_SEQUENCE[step_stage][ 2] );
+  digitalWrite( stepper_pins[3], STEP_SEQUENCE[step_stage][ 3] );
   digitalWrite( stepper_pins[0], STEP_SEQUENCE[step_stage][ 0] );
   digitalWrite( stepper_pins[1], STEP_SEQUENCE[step_stage][ 1] );
   digitalWrite( stepper_pins[2], STEP_SEQUENCE[step_stage][ 2] );
@@ -73,6 +99,22 @@ int stepXY(uint8_t* stepper_pins, unsigned int current_step, bool clockwise){
     current_step -= 1;
   }
   return current_step;
+}
+
+int stepZ(uint8_t* stepper_pinsZ, unsigned int current_step_z, bool clockwise){
+  int step_stage_z = current_step_z % 4;
+  digitalWrite( stepper_pinsZ[0], STEP_SEQUENCE_Z[step_stage_z][ 0] );
+  digitalWrite( stepper_pinsZ[1], STEP_SEQUENCE_Z[step_stage_z][ 1] );
+  digitalWrite( stepper_pinsZ[2], STEP_SEQUENCE_Z[step_stage_z][ 2] );
+  digitalWrite( stepper_pinsZ[3], STEP_SEQUENCE_Z[step_stage_z][ 3] );
+  delay(100);
+  if (clockwise) {
+    current_step_z += 1;
+  }
+  else {
+    current_step_z -= 1;
+  }
+  return current_step_z;
 }
 
 void resetXY(uint8_t* stepper_pins, int current_step_position, bool turn_clockwise){
@@ -110,12 +152,19 @@ void queryDetectors(float* measurements, uint8_t* detector_pins, int num_detecto
 /*************************************************************************/
 void collectSlice(float sinogram[NUM_STEPS_PER_SLICE][NUM_DETECTORS]){
   unsigned int current_xy_step = 0;
-  
-  while (current_xy_step < NUM_STEPS_PER_SLICE){
-    current_xy_step = stepXY(STEPPER_XY_PINS, current_xy_step, CLOCKWISE_DIRECTION);
-    queryDetectors(sinogram[current_xy_step], DETECTOR_PINS, NUM_DETECTORS,
-                   NUM_SAMPLES_PER_DETECTOR);
+  unsigned int current_z_step = 0;
+
+  while (current_z_step < NUM_STEPS_PER_SLICE_Z){
+
+    current_z_step = stepZ(STEPPER_Z_PINS, current_z_step, CLOCKWISE_DIRECTION);
                    delay(100);
+    while (current_xy_step < NUM_STEPS_PER_SLICE){
+      current_xy_step = stepXY(STEPPER_XY_PINS, current_xy_step, CLOCKWISE_DIRECTION);
+      queryDetectors(sinogram[current_xy_step], DETECTOR_PINS, NUM_DETECTORS,
+                     NUM_SAMPLES_PER_DETECTOR);
+                     delay(100);
+    }
+    current_xy_step = 0;
   }
   //resetXY(STEPPER_XY_PINS, current_xy_step, CLOCKWISE_DIRECTION);
 }
@@ -123,6 +172,8 @@ void collectSlice(float sinogram[NUM_STEPS_PER_SLICE][NUM_DETECTORS]){
 void setup() {
  //PINS DE STEPPER W
   setupStepper(STEPPER_XY_PINS);
+  setupStepperZ(STEPPER_Z_PINS);
+
   
   //Inicializacion serial
   Serial.begin(9600);
@@ -143,26 +194,10 @@ void loop() {
   }
   delay(500);
   
-    /*stepW();
-    leerDatos(A0, MED);
-    guardarDatos(0,i);
-    leerDatos(A1, MED);
-    guardarDatos(1,i);
-    leerDatos(A2, MED);
-    guardarDatos(2, i);
-    i++;
-    */
-   //Regresar W
-  //Mover en z
-  
-  
   
 }
  
 /*Función LECTURA: 
- * XX toma como parámetro el pin a leer (pinNumber) y número de muestras a tomar (numMeasures)
- * XX lee (SensorValue) y almacena temporalmente para obtener promedio de medidas (voltage[])
- * XX regresa promedio de mediciones que equivalen a un pin en un paso --> un punto del sinograma final
  */ 
 float leerDatos (int pinNumber, int numMeasures){
 
