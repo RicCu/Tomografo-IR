2496ea8d5683633744ef97064f9fd2539803c99c
Ricardo <>
2018-04-27 23:03:28 -0500

    modify matlab code

--- Echo_Arduino.m
+++ Echo_Arduino.m
@@ -10,7 +10,17 @@ while ard.BytesAvailable == 0 % Esperar respuesta
     pause(1);
 end
 disp('READING');
-m = ReadMatrix(ard, 48, 1);
-m
+sinograma = ReadMatrix(ard, 48, 1)';
+img = iradon(sinograma', 7.5);
+subplot(1, 2, 1);
+
+imshow(sinograma);
+title('Sinograma', 'FontWeight', 'bold', 'FontSize', 16);
+ylabel('Proyectores');
+xlabel('Theta');
+subplot(1, 2, 2);
+imshow(img);
+title('Reconstrucción', 'FontWeight', 'bold', 'FontSize', 16);
+
 fclose(ard);
 
